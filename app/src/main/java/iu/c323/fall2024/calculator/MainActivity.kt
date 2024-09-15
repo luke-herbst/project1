37bd827231d1e3e6f4cc05a151c76c03d081dcac
lsherbst <lsherbst@iu.edu>
2024-09-15 14:35:20 -0400

    project2 complete

--- app/src/main/java/iu/c323/fall2024/calculator/MainActivity.kt
+++ app/src/main/java/iu/c323/fall2024/calculator/MainActivity.kt
@@ -6,6 +6,17 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import kotlin.math.min
+import android.util.Log
+import kotlin.math.ln
+import kotlin.math.cos
+import kotlin.math.tan
+import kotlin.math.sin
+import kotlin.math.log10
+
+
+
+
+private const val TAG="MainActivity"
 
 //Main activity class and creation of variables
 class MainActivity : AppCompatActivity() {
@@ -38,12 +49,21 @@ class MainActivity : AppCompatActivity() {
     private lateinit var equals: Button
     private lateinit var decimal: Button
 
-//Initializes app and creates click listeners for each button
+    //Added buttons for layout
+    private var sin: Button? = null
+    private var cos: Button? = null
+    private var tan: Button? = null
+    private var log: Button? = null
+    private var ln: Button? = null
 
+
+    //Initializes app and creates click listeners for each button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate(Bundle?) called")
 
         displayTextView = findViewById(R.id.input)
         displayTextView.text = currentTextView
@@ -67,6 +87,13 @@ class MainActivity : AppCompatActivity() {
         equals = findViewById(R.id.equal_button)
         decimal = findViewById(R.id.decimal_button)
 
+        //Landscape buttons
+        sin = findViewById(R.id.sin_button)
+        cos = findViewById(R.id.cos_button)
+        tan = findViewById(R.id.tan_button)
+        log = findViewById(R.id.log_button)
+        ln = findViewById(R.id.ln_button)
+
         val buttons = listOf(
             one,
             two,
@@ -89,7 +116,10 @@ class MainActivity : AppCompatActivity() {
             equals
         )
 
-        buttons.forEach { button ->
+        val landscapeButtons = listOf(sin, cos, tan, log, ln)
+        val allButtons = buttons + landscapeButtons.filterNotNull()
+
+        allButtons.forEach { button ->
             button.setOnClickListener { view ->
                 onButtonClick(view)
             }
@@ -100,91 +130,137 @@ class MainActivity : AppCompatActivity() {
 
     //Handles button click event
 
+
     private fun onButtonClick(view: View) {
         val button = view as Button
         val buttonText = button.text.toString()
+        Log.d(TAG, "Button pressed: $buttonText")
 
-        //Edge case
-        if(currentTextView == "Error"){
-            currentTextView = "0"
-            firstNumber = null
-            secondNumber = null
-            operation = null
-        }
 
-        if (button.id == R.id.c_button) {
+        // Edge case
+        if (currentTextView == "Error" || currentTextView =="NaN") {
             currentTextView = "0"
             firstNumber = null
             secondNumber = null
             operation = null
             displayTextView.text = currentTextView
-        } else if (button.id == R.id.division_button || button.id == R.id.multiply_button || button.id == R.id.minus_button || button.id == R.id.plus_button) {
-            //Case for basic operation
-            if (currentTextView.isNotEmpty() && firstNumber == null) {
-                firstNumber = currentTextView
-                operation = buttonText
-                displayTextView.text = currentTextView
-                currentTextView = ""
-            //Case for when the user has already clicked an operation (a long calculation)
-            } else if (currentTextView.isNotEmpty() && operation != null) {
-                secondNumber = currentTextView
-                currentTextView = performOperation(
-                    firstNumber!!.toDouble(),
-                    secondNumber!!.toDouble(),
-                    operation!!
-                )
-                operation = buttonText
-                displayTextView.text = currentTextView
-                firstNumber = currentTextView
-                secondNumber = null
-                displayTextView.text = currentTextView
-                currentTextView = ""
-            }
-        } else if (button.id == R.id.equal_button) {
-            if (firstNumber != null && operation != null) {
-                secondNumber = currentTextView
-                currentTextView = performOperation(
-                    firstNumber!!.toDouble(),
-                    secondNumber!!.toDouble(),
-                    operation!!
-                )
+        }
+
+        when (button.id) {
+            R.id.c_button -> {
+                currentTextView = "0"
                 firstNumber = null
                 secondNumber = null
                 operation = null
+                displayTextView.text = currentTextView
             }
-            displayTextView.text = currentTextView
-        } else if (button.id == R.id.decimal_button) {
-            if (!currentTextView.contains(".")) {
-                currentTextView += "."
-            }
-            displayTextView.text = currentTextView
-        } else if (button.id == R.id.negate_button) {
-            if (currentTextView.isNotEmpty()) {
-                currentTextView = if (currentTextView.startsWith("-")) {
-                    currentTextView.substring(1)
-                } else {
-                    "-$currentTextView"
+            R.id.division_button, R.id.multiply_button, R.id.minus_button, R.id.plus_button -> {
+                // Case for basic operation
+                if (currentTextView.isNotEmpty() && firstNumber == null) {
+                    firstNumber = currentTextView
+                    operation = buttonText
+                    displayTextView.text = currentTextView
+                    currentTextView = ""
+                // Case for when the user has already clicked an operation (a long calculation)
+                } else if (currentTextView.isNotEmpty() && operation != null) {
+                    secondNumber = currentTextView
+                    currentTextView = performOperation(
+                        firstNumber!!.toDouble(),
+                        secondNumber!!.toDouble(),
+                        operation!!
+                    )
+                    operation = buttonText
+                    displayTextView.text = currentTextView
+                    firstNumber = currentTextView
+                    secondNumber = null
+                    displayTextView.text = currentTextView
+                    currentTextView = ""
                 }
             }
-            displayTextView.text = currentTextView
-        } else if (button.id == R.id.percent_button) {
-            if (currentTextView.isNotEmpty()) {
-                currentTextView = (currentTextView.toDouble() / 100).toString()
+            R.id.equal_button -> {
+                if (firstNumber != null && operation != null) {
+                    secondNumber = currentTextView
+                    currentTextView = performOperation(
+                        firstNumber!!.toDouble(),
+                        secondNumber!!.toDouble(),
+                        operation!!
+                    )
+                    firstNumber = null
+                    secondNumber = null
+                    operation = null
+                }
+                displayTextView.text = currentTextView
             }
-            displayTextView.text = currentTextView
-        } else {
-            if (currentTextView == "0") {
-                currentTextView = buttonText
-            } else {
-                currentTextView += buttonText
+            R.id.decimal_button -> {
+                if (!currentTextView.contains(".")) {
+                    currentTextView += "."
+                }
+                displayTextView.text = currentTextView
             }
-            displayTextView.text = currentTextView
+            R.id.negate_button -> {
+                if (currentTextView.isNotEmpty()) {
+                    currentTextView = if (currentTextView.startsWith("-")) {
+                        currentTextView.substring(1)
+                    } else {
+                        "-$currentTextView"
+                    }
+                }
+                displayTextView.text = currentTextView
+            }
+            R.id.percent_button -> {
+                if (currentTextView.isNotEmpty()) {
+                    currentTextView = (currentTextView.toDouble() / 100).toString()
+                }
+                displayTextView.text = currentTextView
+            }
+            R.id.ln_button -> {
+                if(currentTextView=="0"){
+                    currentTextView="Error"
+                } else if(currentTextView.isNotEmpty()){
+                    currentTextView=ln(currentTextView.toDouble()).toString()
+                }
+                displayTextView.text = currentTextView
+            }
+            R.id.sin_button -> {
+                if(currentTextView.isNotEmpty()){
+                    currentTextView=sin(currentTextView.toDouble()).toString()
+                }
+                displayTextView.text = currentTextView
+            }
+            R.id.cos_button -> {
+                if(currentTextView.isNotEmpty()){
+                    currentTextView=cos(currentTextView.toDouble()).toString()
+                }
+                displayTextView.text = currentTextView
+            }
+            R.id.tan_button -> {
+                if(currentTextView.isNotEmpty()){
+                    currentTextView=tan(currentTextView.toDouble()).toString()
+                }
+                displayTextView.text = currentTextView
+            }
+            R.id.log_button -> {
+                if(currentTextView.isNotEmpty()){
+                    currentTextView=log10(currentTextView.toDouble()).toString()
+                }
+                displayTextView.text = currentTextView
+            }
+            else -> {
+                if (currentTextView == "0") {
+                    currentTextView = buttonText
+                } else {
+                    currentTextView += buttonText
+                }
+                displayTextView.text = currentTextView
+            }
         }
     }
+
 
 // Calculates operation
 
     private fun performOperation(num1: Double, num2: Double, op: String): String {
+        Log.d("CalculatorApp", "operation performed")
         //Edge case for division by zero
         if(op=="/" && num2 == 0.0){
             return "Error"
@@ -197,5 +273,50 @@ class MainActivity : AppCompatActivity() {
             else -> ""
         }
     }
+
+    //Save instance to handle rotation
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("currentTextView", currentTextView)
+        outState.putString("firstNumber", firstNumber)
+        outState.putString("secondNumber", secondNumber)
+        outState.putString("operation", operation)
+    }
+
+    //Restores instance state
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        currentTextView = savedInstanceState.getString("currentTextView", "0")
+        firstNumber = savedInstanceState.getString("firstNumber")
+        secondNumber = savedInstanceState.getString("secondNumber")
+        operation = savedInstanceState.getString("operation")
+        displayTextView.text = currentTextView
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
 
 }
